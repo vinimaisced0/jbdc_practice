8c20cfc7367aff5fc593a1d87d1b833a44912333
Vinícius Macêdo <vinimaiscedo08@gmail.com>
2025-11-19 10:47:03 -0300

    updating db credentials

--- src/Conexao.java
+++ src/Conexao.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
  class Conexao {
 
      private static final String URL = "jdbc:mysql://localhost:3306/bd";
-     private static final String USUARIO = "root";
-     private static final String SENHA = "admin";
+     private static final String USUARIO = "app_user";
+     private static final String SENHA = "senha123";
 
 
     public static Connection getConexao() throws SQLException {
